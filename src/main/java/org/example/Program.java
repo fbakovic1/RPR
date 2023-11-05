6db4ad9d019280a7186a2e4da06078f481dbd36f
Faruk Bakovic <farukbakovic02@gmail.com>
2023-11-05 13:48:57 +0100

    Initial commit

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Program {
     public static void main( String[] args )
     {
+        //
         Scanner unos = new Scanner(System.in);
         Imenik imenik = new Imenik();
     }
