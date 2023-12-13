ec75540bc835d7e7d04f64177aa5c97c5448575b
Faruk Bakovic <farukbakovic02@gmail.com>
2023-12-13 09:53:57 +0100

    Handle user selection.

--- src/main/java/ba/unsa/etf/rpr/t7z1/HelloController.java
+++ src/main/java/ba/unsa/etf/rpr/t7z1/HelloController.java
@@ -1,9 +1,62 @@
 package ba.unsa.etf.rpr.t7z1;
 
-
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.collections.ObservableList;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
+
+    public ListView listaKorisnika;
+    public TextField imeId;
+    public TextField prezimeId;
+    public TextField emailId;
+    public TextField usernameId;
+    public PasswordField passwordId;
+    public Button krajBtn;
+    public Button dodajBtn;
+
+    private ObservableList<Korisnik> korisniciList = FXCollections.observableArrayList();
+
+
+    @FXML
+    public void DodajKorisnika(ActionEvent event) {
+        Korisnik korisnik = new Korisnik(imeId.getText(), prezimeId.getText(), emailId.getText(), usernameId.getText(), passwordId.getText());
+
+        korisniciList.add(korisnik);
+
+        listaKorisnika.setItems(korisniciList);
+
+        imeId.clear();
+        prezimeId.clear();
+        emailId.clear();
+        usernameId.clear();
+        passwordId.clear();
+    }
+
+    @FXML
+    public void ZavrsiProgram(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    @FXML
+    public void popuniPodatkeKorisnika(MouseEvent mouseEvent) {
+        listaKorisnika.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Korisnik selectedKorisnik = (Korisnik) listaKorisnika.getSelectionModel().getSelectedItem();
+                if (selectedKorisnik != null) {
+                    imeId.setText(selectedKorisnik.imeProperty().getValue());
+                    prezimeId.setText(selectedKorisnik.prezimeProperty().getValue());
+                    emailId.setText(selectedKorisnik.emailProperty().getValue());
+                    usernameId.setText(selectedKorisnik.usernameProperty().getValue());
+                    passwordId.setText(selectedKorisnik.passwordProperty().getValue());
+                }
+            }
+        });
+    }
 
 }
--- src/main/java/ba/unsa/etf/rpr/t7z1/Korisnik.java
+++ src/main/java/ba/unsa/etf/rpr/t7z1/Korisnik.java
@@ -1,70 +1,65 @@
 package ba.unsa.etf.rpr.t7z1;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Korisnik {
-    private String ime, prezime, email, username, password;
+    private SimpleStringProperty ime;
+    private SimpleStringProperty prezime;
+    private SimpleStringProperty email;
+    private SimpleStringProperty username;
+    private SimpleStringProperty password;
 
-    public Korisnik(String ime, String prezime, String username, String password) {
-        this.ime = ime;
-        this.prezime = prezime;
-        this.username = username;
-        this.password = password;
-    }
-
-    public Korisnik() {
+    public Korisnik(String ime, String prezime, String email, String username, String password) {
+        this.ime = new SimpleStringProperty(ime);
+        this.prezime = new SimpleStringProperty(prezime);
+        this.email = new SimpleStringProperty(email);
+        this.username = new SimpleStringProperty(username);
+        this.password = new SimpleStringProperty(password);
     }
 
     public String getIme() {
+        return ime.get();
+    }
+
+    public SimpleStringProperty imeProperty() {
         return ime;
     }
 
-    public void setIme(String ime) {
-        if (ime == null) throw new IllegalArgumentException("Ime ne može biti null");
-        this.ime = ime;
+    public String getPrezime() {
+        return prezime.get();
     }
 
-    public String getPrezime() {
+    public SimpleStringProperty prezimeProperty() {
         return prezime;
     }
 
-    public void setPrezime(String prezime) {
-        if (prezime == null) throw new IllegalArgumentException("Prezime ne može biti null");
-        this.prezime = prezime;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        if (username == null) throw new IllegalArgumentException("Username ne može biti null");
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        if (password == null) throw new IllegalArgumentException("Password ne može biti null");
-        this.password = password;
-    }
-
     public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
         return email;
     }
 
-    public void setEmail(String email) {
-        if (email == null) throw new IllegalArgumentException("Email ne može biti null");
-        this.email = email;
+    public String getUsername() {
+        return username.get();
     }
 
-    @Override
-    public String toString() {
-        return "Korisnik{" +
-                "ime='" + ime + '\'' +
-                ", prezime='" + prezime + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public SimpleStringProperty usernameProperty() {
+        return username;
     }
+
+    public String getPassword() {
+        return password.get();
+    }
+
+    public SimpleStringProperty passwordProperty() {
+        return password;
+    }
+
+    public String toString() {
+        return ime.get() + " " + prezime.get();
+    }
+
+
 }
